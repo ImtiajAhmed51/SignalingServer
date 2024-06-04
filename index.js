eb761ff861ca244761ab57cfa3b075f7f13d59d4
Imtiaj Ahmed Anik <87453627+ImtiajAhmed51@users.noreply.github.com>
2024-06-04 21:40:59 +0000

    back

--- index.js
+++ index.js
@@ -1,84 +1,70 @@
-"use strict";
+'use strict';
 
-var os = require("os");
-var nodeStatic = require("node-static");
-var http = require("http");
-var socketIO = require("socket.io");
+const os = require('os');
+const nodeStatic = require('node-static');
+const http = require('http');
+const socketIO = require('socket.io');
 const port = process.env.PORT || 4000;
 
-var fileServer = new nodeStatic.Server();
-
-// Create HTTP server and handle requests
-var app = http.createServer(function (req, res) {
-  fileServer.serve(req, res, function (err, result) {
-    if (err) {
-      if (!res.headersSent) {
-        console.error("Error serving file:", err);
-        res.writeHead(err.status, err.headers);
-      }
-      res.end();
-    }
-  });
-}).listen(port, function() {
-  console.log(`Server is listening on port ${port}`);
+const fileServer = new nodeStatic.Server();
+const app = http.createServer((req, res) => {
+  fileServer.serve(req, res);
+}).listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+}).on('error', (err) => {
+  console.error(`Server error: ${err}`);
 });
 
-// Initialize socket.io
-var io = socketIO.listen(app);
+let users = {};
 
-io.sockets.on("connection", function (socket) {
-  // Convenience function to log server messages on the client
-  function log() {
-    var array = ["Message from server:"];
-    array.push.apply(array, arguments);
-    socket.emit("log", array);
+const io = socketIO(app, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
   }
+});
 
-  // Handle 'message' event
-  socket.on("message", function (message) {
-    log("Client said: ", message);
-    // For a real app, would be room-only (not broadcast)
-    socket.broadcast.emit("message", message);
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('register', (username) => {
+    users[username] = socket.id;
+    console.log(`User registered: ${username}`);
+    io.emit('user_list', Object.keys(users));
   });
 
-  // Handle 'create or join' event
-  socket.on("create or join", function (room) {
-    log("Received request to create or join room " + room);
-
-    var clientsInRoom = io.sockets.adapter.rooms[room];
-    var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
-    log("Room " + room + " now has " + numClients + " client(s)");
-
-    if (numClients === 0) {
-      socket.join(room);
-      log("Client ID " + socket.id + " created room " + room);
-      socket.emit("created", room, socket.id);
-    } else if (numClients === 1) {
-      log("Client ID " + socket.id + " joined room " + room);
-      io.sockets.in(room).emit("join", room);
-      socket.join(room);
-      socket.emit("joined", room, socket.id);
-      io.sockets.in(room).emit("ready");
+  socket.on('call', (data) => {
+    const { from, to } = data;
+    if (users[to]) {
+      console.log(`Call from ${from} to ${to}`);
+      io.to(users[to]).emit('incoming_call', { from });
     } else {
-      // Max two clients
-      socket.emit("full", room);
+      console.log(`User ${to} not found`);
     }
   });
 
-  // Handle 'ipaddr' event
-  socket.on("ipaddr", function () {
-    var ifaces = os.networkInterfaces();
-    for (var dev in ifaces) {
-      ifaces[dev].forEach(function (details) {
-        if (details.family === "IPv4" && details.address !== "127.0.0.1") {
-          socket.emit("ipaddr", details.address);
-        }
-      });
+  socket.on('accept_call', (data) => {
+    const { from, to } = data;
+    if (users[from]) {
+      console.log(`Call accepted by ${to} from ${from}`);
+      io.to(users[from]).emit('call_accepted', { to });
+    } else {
+      console.log(`User ${from} not found`);
     }
   });
 
-  // Handle 'bye' event
-  socket.on("bye", function () {
-    console.log("Received bye");
+  socket.on('disconnect', () => {
+    for (const [username, socketId] of Object.entries(users)) {
+      if (socketId === socket.id) {
+        console.log(`User disconnected: ${username}`);
+        delete users[username];
+        io.emit('user_list', Object.keys(users));
+        break;
+      }
+    }
+  });
+
+  socket.on('error', (err) => {
+    console.error(`Socket error: ${err}`);
   });
 });
