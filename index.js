08cd12863cee159007c9e0dd39fb84cb4b0e9d03
Imtiaj Ahmed Anik <87453627+ImtiajAhmed51@users.noreply.github.com>
2024-06-04 20:02:50 +0000

    error fix

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+'use strict';
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+var os = require('os');
+var nodeStatic = require('node-static');
+var http = require('http');
+var socketIO = require('socket.io');
+const port = process.env.PORT || 4000;
+
+var fileServer = new(nodeStatic.Server)();
+var app = http.createServer(function(req, res) {
+  fileServer.serve(req, res);
+}).listen(port);
 
 let users = {};
 
-io.on('connection', (socket) => {
-    socket.on('register', (username) => {
-        users[username] = socket.id;
-        io.emit('user_list', Object.keys(users));
-    });
+var io = socketIO.listen(app);
+io.sockets.on('connection', function(socket) {
 
-    socket.on('call', (data) => {
-        const { from, to } = data;
-        if (users[to]) {
-            io.to(users[to]).emit('incoming_call', { from });
-        }
-    });
+  socket.on('register', (username) => {
+    users[username] = socket.id;
+    io.emit('user_list', Object.keys(users));
+  });
 
-    socket.on('accept_call', (data) => {
-        const { from, to } = data;
-        if (users[from]) {
-            io.to(users[from]).emit('call_accepted', { to });
-        }
-    });
+  socket.on('call', (data) => {
+    const { from, to } = data;
+    if (users[to]) {
+        io.to(users[to]).emit('incoming_call', { from });
+    }
+  });
 
-    socket.on('disconnect', () => {
-        for (const [username, socketId] of Object.entries(users)) {
-            if (socketId === socket.id) {
-                delete users[username];
-                io.emit('user_list', Object.keys(users));
-                break;
-            }
+  socket.on('accept_call', (data) => {
+    const { from, to } = data;
+    if (users[from]) {
+        io.to(users[from]).emit('call_accepted', { to });
+    }
+  });
+
+  socket.on('disconnect', () => {
+    for (const [username, socketId] of Object.entries(users)) {
+        if (socketId === socket.id) {
+            delete users[username];
+            io.emit('user_list', Object.keys(users));
+            break;
         }
-    });
+    }
+  });
+
+
+
+
 });
 
-const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
-    console.log(`Server listening on port a ${PORT}`);
-});
