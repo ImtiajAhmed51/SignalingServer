e7c2aea385cbe33119407e76c40e499d38fe15ef
Imtiaj Ahmed Anik <87453627+ImtiajAhmed51@users.noreply.github.com>
2024-06-04 19:33:35 +0000

    change

--- index.js
+++ index.js
@@ -1,83 +1,45 @@
-"use strict";
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-var os = require("os");
-var nodeStatic = require("node-static");
-var http = require("http");
-var socketIO = require("socket.io");
-const port = process.env.PORT || 4000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-var fileServer = new nodeStatic.Server();
+let users = {};
 
-// Create HTTP server and handle requests
-var app = http.createServer(function (req, res) {
-  fileServer.serve(req, res, function (err, result) {
-    if (err) {
-      if (!res.headersSent) {
-        console.error("Error serving file:", err);
-        res.writeHead(err.status, err.headers);
-      }
-      res.end();
-    }
-  });
-}).listen(port, function() {
-  console.log(`Server is listening on port ${port}`);
+io.on('connection', (socket) => {
+    socket.on('register', (username) => {
+        users[username] = socket.id;
+        io.emit('user_list', Object.keys(users));
+    });
+
+    socket.on('call', (data) => {
+        const { from, to } = data;
+        if (users[to]) {
+            io.to(users[to]).emit('incoming_call', { from });
+        }
+    });
+
+    socket.on('accept_call', (data) => {
+        const { from, to } = data;
+        if (users[from]) {
+            io.to(users[from]).emit('call_accepted', { to });
+        }
+    });
+
+    socket.on('disconnect', () => {
+        for (const [username, socketId] of Object.entries(users)) {
+            if (socketId === socket.id) {
+                delete users[username];
+                io.emit('user_list', Object.keys(users));
+                break;
+            }
+        }
+    });
 });
 
-var io = socketIO.listen(app);
-
-io.sockets.on("connection", function (socket) {
-  // Convenience function to log server messages on the client
-  function log() {
-    var array = ["Message from server:"];
-    array.push.apply(array, arguments);
-    socket.emit("log", array);
-  }
-
-  // Handle 'message' event
-  socket.on("message", function (message) {
-    log("Client said: ", message);
-    // For a real app, would be room-only (not broadcast)
-    socket.broadcast.emit("message", message);
-  });
-
-  // Handle 'create or join' event
-  socket.on("create or join", function (room) {
-    log("Received request to create or join room " + room);
-
-    var clientsInRoom = io.sockets.adapter.rooms[room];
-    var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
-    log("Room " + room + " now has " + numClients + " client(s)");
-
-    if (numClients === 0) {
-      socket.join(room);
-      log("Client ID " + socket.id + " created room " + room);
-      socket.emit("created", room, socket.id);
-    } else if (numClients === 1) {
-      log("Client ID " + socket.id + " joined room " + room);
-      io.sockets.in(room).emit("join", room);
-      socket.join(room);
-      socket.emit("joined", room, socket.id);
-      io.sockets.in(room).emit("ready");
-    } else {
-      // Max two clients
-      socket.emit("full", room);
-    }
-  });
-
-  // Handle 'ipaddr' event
-  socket.on("ipaddr", function () {
-    var ifaces = os.networkInterfaces();
-    for (var dev in ifaces) {
-      ifaces[dev].forEach(function (details) {
-        if (details.family === "IPv4" && details.address !== "127.0.0.1") {
-          socket.emit("ipaddr", details.address);
-        }
-      });
-    }
-  });
-
-  // Handle 'bye' event
-  socket.on("bye", function () {
-    console.log("Received bye");
-  });
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
