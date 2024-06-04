71a0e54213d7eeb80a564c2e72c5ab042c6ac992
Imtiaj Ahmed Anik <87453627+ImtiajAhmed51@users.noreply.github.com>
2024-06-04 19:46:22 +0000

    update

--- index.js
+++ index.js
@@ -41,5 +41,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port a ${PORT}`);
 });
